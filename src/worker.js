4dc2b24c0ecea3cd8b5723df55acf3dd84a07e80
abibv <anandabv1206@gmail.com>
2021-07-17 03:51:25 +0530

    first commit

---
+++ src/worker.js
@@ -0,0 +1,41 @@
+self.onmessage = (e) => {
+  const { data } = e;
+  switch(data.action) {
+    case 'bubbleSort': 
+      const dt = bubbleSort();
+      self.postMessage(dt);
+      break;
+    default: break;
+  }
+};
+
+const bubbleSort = () => {
+  var a = [];
+  const beforePush = performance.now();
+  for (var i = 50000; i >= 0; i--) {
+      a.push(i);
+  };
+  const afterPush = performance.now();
+  const beforeSort = performance.now();
+  var swapped;
+  do {
+      swapped = false;
+      for (var i=0; i < a.length-1; i++) {
+          if (a[i] > a[i+1]) {
+              var temp = a[i];
+              a[i] = a[i+1];
+              a[i+1] = temp;
+              swapped = true;
+          }
+      }
+  } while (swapped);
+  const afterSort = performance.now();
+  const loadTime = afterPush - beforePush;
+  const sortTime = afterSort - beforeSort;
+  const totalTime = loadTime + sortTime;
+  return {
+    loadTime,
+    sortTime,
+    totalTime,
+  };
+};
